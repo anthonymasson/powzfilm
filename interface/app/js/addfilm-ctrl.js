91114aa762a6bfbafbd8336cb92bc5c55b71cf80
Masson Anthony <ama@reportlinker.com>
2015-03-30 17:53:16 +0200

    select imdb++

--- interface/app/js/addfilm-ctrl.js
+++ interface/app/js/addfilm-ctrl.js
@@ -1,19 +1,35 @@
 ////------------[Controllers]
+function Movie(id_imdb, i_movie){
+    if (angular.isDefined(i_movie)){
+        this.title = i_movie.title;
+        this.poster_path = i_movie.poster_path;
+        this.genres = [];
+        for (var g in i_movie.genres)
+            this.genres[this.genres.length] = i_movie.genres[g];
+        this.synopsis = i_movie.overview;
+        this.spoken_languages = [];
+        for (var l in i_movie.spoken_languages)
+            this.spoken_languages[this.spoken_languages.length] = i_movie.spoken_languages[l];
+        this.imdb_id = id_imdb;
+        this.release_date = i_movie.release_date;
+    }
+}
+
 powzfilmControllers.controller('AddFilmCtrl', ['$scope', '$location', '$routeParams', '$http', '$timeout',
     function ($scope, $location, $routeParams, $http, $timeout) {
         var nb_elem_row = 4;
         $scope.selected_imdb = false;
         $scope.search_imdb = "harry";
         $scope.imdb_res = [];
-        
         $scope.select_imdb = function(id){
-            $scope.selected_imdb = id;
+            $http.get(API_URLS.detail_movie.replace('<id_movie>', id)).success(function (res){
+                $scope.selected_imdb = id;
+                $scope.new_movie = new Movie(id, res);
+            }).error(function (res){
+               console.log(res); 
+            });
         };
-        
-        $scope.change_selected_imdb = function(){
-            $scope.selected_imdb = false;
-        };
-        
+        $scope.change_selected_imdb = function(){$scope.selected_imdb = false;};
         var filterTextTimeout;
         $scope.$watch('search_imdb', function (val) {
             if (filterTextTimeout) $timeout.cancel(filterTextTimeout);
@@ -21,7 +37,9 @@ powzfilmControllers.controller('AddFilmCtrl', ['$scope', '$location', '$routePar
                 $scope.page = 1;
                 if ($scope.search_imdb.length > LIMIT_LENGTH_SEARCH){
                     $scope.selected_imdb = null;
-                    $http.get(API_URLS.search_movie.replace('<search>', $scope.search_imdb).replace('<page>', $scope.page))
+                    $http.get(API_URLS.search_movie
+                            .replace('<search>', $scope.search_imdb)
+                            .replace('<page>', $scope.page))
                         .success(function (res){
                             var results = {};
                             for (var r in res.results)
@@ -29,7 +47,7 @@ powzfilmControllers.controller('AddFilmCtrl', ['$scope', '$location', '$routePar
                             res.results = results;
                             $scope.imdb_res = res;
                         }).error(function (res){
-                            $scope.imdb_res = res;
+                            console.log(res);
                         });
                     } else {
                         $scope.imdb_res = [];
