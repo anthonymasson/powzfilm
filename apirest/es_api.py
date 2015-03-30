d212ee6b132c7adf8c9cec27899495361299f6ee
Masson Anthony <ama@reportlinker.com>
2015-03-30 11:55:03 +0200

    PEP 8 convention

--- apirest/es_api.py
+++ apirest/es_api.py
@@ -3,63 +3,75 @@ from properties import *
 import json as j
 import requests as r
 
+
 @hook('after_request')
 def enable_cors():
     response.headers['Access-Control-Allow-Origin'] = '*'
     response.headers['Access-Control-Allow-Methods'] = 'PUT, GET, POST, DELETE, OPTIONS'
     response.headers['Access-Control-Allow-Headers'] = 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token'
 
-#TEST working
+
+# TEST working
 @route('/')
 def get_test():
     log_api.info(request.method + '/')
     return j.dumps({'test': 'ok'})
 
-#---- MDB SEARCH
-@route('/mdb/search/movie/<search>')
-@route('/mdb/search/movie/<search>/<page>')
-def get_mdb_movie_search(search, page="1"):
-    log_api.info(request.method + '/mdb/movie/' + search + '/' + page)
-    return j.dumps(r.get(MovieDB.Urls.search_movie.replace('{query}', search).replace('{page}', page)).json())
 
-
+# MDB SEARCH
+# MDB TV
 @route('/mdb/search/tv/<search>')
 @route('/mdb/search/tv/<search>/<page>')
 def get_mdb_tv_search(search, page="1"):
     log_api.info(request.method + '/mdb/tv/' + search + '/' + page)
-    return j.dumps(r.get(MovieDB.Urls.search_tv.replace('{query}', search).replace('{page}', page)).json())
+    return j.dumps(r.get(MovieDB.search_tv.replace('{query}', search).replace('{page}', page)).json())
 
 
-@route('/mdb/movie/<id>')
-def get_mdb_movie_detail(id):
-    log_api.info(request.method + '/mdb/movie/' + id)
-    return j.dumps(r.get(MovieDB.Urls.detail_movie.replace('{id}', id)).json())
+@route('/mdb/poster/tv/<id_movie>')
+def get_mdb_poster_movie_detail(id_movie):
+    log_api.info(request.method + '/mdb/movie/' + id_movie)
+    tv = r.get(MovieDB.detail_tv.replace('{id}', id_movie)).json()
+    return j.dumps({'path': MovieDB.poster_185 + tv['poster_path'], 'id': id_movie})
 
 
-@route('/mdb/poster/movie/<id>')
-def get_mdb_poster_movie_detail(id):
-    log_api.info(request.method + '/mdb/movie/' + id)
-    return j.dumps({'path': MovieDB.Urls.poster_185 + r.get(MovieDB.Urls.detail_movie.replace('{id}', id)).json()['poster_path'], 'id': id})
+@route('/mdb/tv/genres')
+def get_mdb_tv_genres():
+    log_api.info(request.method + '/mdb/tv/genres')
+    return j.dumps(r.get(MovieDB.genre_tv_list).json())
 
 
-@route('/mdb/poster/tv/<id>')
-def get_mdb_poster_movie_detail(id):
-    log_api.info(request.method + '/mdb/movie/' + id)
-    return j.dumps({'path': MovieDB.Urls.poster_185 + r.get(MovieDB.Urls.detail_tv.replace('{id}', id)).json()['poster_path'], 'id': id})
+# MDB Movie
+@route('/mdb/search/movie/<search>')
+@route('/mdb/search/movie/<search>/<page>')
+def get_mdb_movie_search(search, page="1"):
+    log_api.info(request.method + '/mdb/movie/' + search + '/' + page)
+    return j.dumps(r.get(MovieDB.search_movie.replace('{query}', search).replace('{page}', page)).json())
+
+
+@route('/mdb/movie/<id_movie>')
+def get_mdb_movie_detail(id_movie):
+    log_api.info(request.method + '/mdb/movie/' + id_movie)
+    return j.dumps(r.get(MovieDB.detail_movie.replace('{id}', id_movie)).json())
+
+
+@route('/mdb/poster/movie/<id_movie>')
+def get_mdb_poster_movie_detail(id_movie):
+    log_api.info(request.method + '/mdb/movie/' + id_movie)
+    movie = r.get(MovieDB.detail_movie.replace('{id}', id_movie)).json()
+    return j.dumps({'path': MovieDB.poster_185 + movie['poster_path'], 'id': id_movie})
 
 
 @route('/mdb/movie/genres')
 def get_mdb_movie_genres():
     log_api.info(request.method + '/mdb/movie/genres')
-    return j.dumps(r.get(MovieDB.Urls.genre_movie_list).json())
+    return j.dumps(r.get(MovieDB.genre_movie_list).json())
 
 
-@route('/mdb/genre/<id>/movie/')
-@route('/mdb/genre/<id>/movie/<page>')
-def get_mdb_tv_genres(id, page="1"):
-    log_api.info(request.method + '/mdb/genre/' + id + '/movie/' + page)
-    return j.dumps(r.get(MovieDB.Urls.movie_by_genre_list.replace('{id}', id).replace('{page}', page)).json())
-#---- MDB SEARCH
+@route('/mdb/genre/<id_genre>/movie')
+@route('/mdb/genre/<id_genre>/movie/<page>')
+def get_mdb_movie_genres(id_genre, page="1"):
+    log_api.info(request.method + '/mdb/genre/' + id_genre + '/movie/' + page)
+    return j.dumps(r.get(MovieDB.movie_by_genre_list.replace('{id}', id_genre).replace('{page}', page)).json())
 
-#Start api
+# Start api
 run(host='0.0.0.0', port=config.port, debug=True)
--- apirest/properties.py
+++ apirest/properties.py
@@ -2,7 +2,7 @@ from api_key import API_KEY
 import logging
 import argparse
 
-#Arugment parsing
+# Argument parsing
 parser = argparse.ArgumentParser(description='Get images from site')
 parser.add_argument('-eshost', '-e', type=str, help="host es", default='127.0.0.1:9200')
 parser.add_argument('-index', '-i', type=str, help="index es", default='powzfilm')
@@ -10,39 +10,39 @@ parser.add_argument('-port', '-p', type=int, help="server port", default=4242)
 parser.add_argument('-log', '-l', type=str, help="log filename", default='powzfilm.log')
 config = parser.parse_args()
 
-
-#Logger
-logger = logging.getLogger('powzfilm') #fichier
-log_api = logging.getLogger('powzfilm') #console
+# Logger
+logger = logging.getLogger('powzfilm')   # File
+log_api = logging.getLogger('powzfilm')  # Console
 logger.setLevel(logging.DEBUG)
-logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
-                    datefmt='%Y-%m-%d %H:%M:%S',
-                    filename=config.log, filemode='w')
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S', filename=config.log, filemode='w')
 logging.getLogger("requests").setLevel(logging.WARNING)
 console = logging.StreamHandler()
 console.setLevel(logging.INFO)
 console.setFormatter(logging.Formatter('%(name)-5s: %(levelname)-8s %(message)s'))
 logging.getLogger('').addHandler(console)
 
-#Complementary Information
+# Complementary Information
 config.es = config.eshost + '/' + config.index + '/'
 
+
 class MovieDB:
+    def __init__(self):
+        pass
+
     api_key = API_KEY
     api = 'http://api.themoviedb.org/3'
-    class Urls:
-        pass
-    Urls.genre_movie_list = api + '/genre/movie/list?api_key=' + api_key
-    Urls.movie_by_genre_list = api + '/genre/{id}/movie/list?page={page}&api_key=' + api_key
-    Urls.genre_tv_list = api + '/genre/tv/list?api_key=' + api_key
-    Urls.search_movie = api + '/search/movie?query={query}&page={page}&api_key=' + api_key
-    Urls.detail_movie = api + '/movie/{id}?api_key=' + api_key
-    Urls.search_tv = api + '/search/tv?query={query}&page={page}&api_key=' + api_key
-    Urls.detail_tv = api + '/tv/{id}?api_key=' + api_key
-    Urls.poster_185 = 'https://image.tmdb.org/t/p/w185'
-    Urls.poster_500 = 'https://image.tmdb.org/t/p/w500'
+    # TV urls
+    genre_tv_list = api + '/genre/tv/list?api_key=' + api_key
+    search_tv = api + '/search/tv?query={query}&page={page}&api_key=' + api_key
+    detail_tv = api + '/tv/{id}?api_key=' + api_key
+    # Movie urls
+    genre_movie_list = api + '/genre/movie/list?api_key=' + api_key
+    movie_by_genre_list = api + '/genre/{id}/movie/list?page={page}&api_key=' + api_key
+    search_movie = api + '/search/movie?query={query}&page={page}&api_key=' + api_key
+    detail_movie = api + '/movie/{id}?api_key=' + api_key
+    poster_185 = 'https://image.tmdb.org/t/p/w185'
+    poster_500 = 'https://image.tmdb.org/t/p/w500'
 
-#Print config
-log_api.info('Start with ES : ' + config.es);
-
+# Print config
+log_api.info('Start with ES : ' + config.es)
